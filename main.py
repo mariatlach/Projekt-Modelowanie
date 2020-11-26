d5687d4941ecdbfddcd2f1831545df15b8d0d4b4
Maria <mariateresalachowicz@gmail.com>
2020-11-26 12:53:12 +0100

    Pierwszo tygodniowy commit

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-
-
+# -*- coding: utf-8 -*-
+import ctypes
+import codecs
+# tutaj jest mala funkcja ktora otwiera nasze okienko z wyjasnieniem Z POLSKIMI ZNAKAMI :))))))
+popup = ''
+with codecs.open('fotokom.txt', encoding='utf-8') as f:
+    for line in f:
+        popup = popup + line
+ctypes.windll.user32.MessageBoxW(0, popup, "Wyjasnienie", 1)
